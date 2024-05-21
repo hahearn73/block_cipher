85a9431e59e485dcee8a050d2bd8708cf3a8e298
hahearn730 <hahearn730@gmail.com>
2024-05-21 17:43:58 -0400

    first commit

---
+++ block_cipher.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define BLOCK_SIZE 1024
+#define KEY_LENGTH 1
+
+void encrypt(FILE *input_file, FILE *output_file, uint8_t key) {
+    return;
+}
+
+void decrypt(FILE *input_file, FILE *output_file, uint8_t key) {
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    // usage check
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s <-e|-d> <source_file> <key> <destination_file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    // get command line args
+    const char *flag = argv[1];
+    const char *input_filename = argv[2];
+    const char *key_str = argv[3];
+    const char *output_filename = argv[4];
+
+    // open files
+    FILE *input_file = fopen(input_filename, "rb");
+    FILE *output_file = fopen(output_filename, "wb");
+    if (input_file == NULL) {
+        perror("Error opening input file");
+        return EXIT_FAILURE;
+    }
+    if (output_file == NULL) {
+        perror("Error opening output file");
+        return EXIT_FAILURE;
+    }
+
+    // validate flag
+    if (strcmp(flag, "-e") != 0 && strcmp(flag, "-d") != 0) {
+        fprintf(stderr, "Invalid flag. Use -e for encryption or -d for decryption.\n");
+        return EXIT_FAILURE;
+    }
+
+    // validate key
+    if (strlen(key_str) != KEY_LENGTH) {
+        fprintf(stderr, "Invalid key. Key must be of length 1\n");
+        return EXIT_FAILURE;        
+    }
+    uint8_t key = key_str[0];
+
+    // perform encryption/decryption
+    if (strcmp(flag, "-e") == 0) {
+        encrypt(input_file, output_file, key);
+    }
+    else {
+        decrypt(input_file, output_file, key);
+    }
+
+    if (ferror(input_file)) {
+        perror("Error reading input file");
+        fclose(input_file);
+        return EXIT_FAILURE;
+    }
+    fclose(input_file);
+
+    if (ferror(output_file)) {
+        perror("Error reading output file");
+        fclose(output_file);
+        return EXIT_FAILURE;
+    }
+    fclose(output_file);
+
+    return EXIT_SUCCESS;
+}
